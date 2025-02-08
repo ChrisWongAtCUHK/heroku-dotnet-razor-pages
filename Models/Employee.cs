d6719538b9f93ee0bafc9cfbbf1cd22075a7f751
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-08 19:41:13 +0800

    ASP.NET Core razor pages validation

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -5,9 +5,12 @@ namespace DotNetRazorPages.Models;
 public class Employee
 {
   public int Id { get; set; }
-   [Required(ErrorMessage = "Name is required")]
+  [Required(ErrorMessage = "Name is required"), MinLength(3, ErrorMessage = "Name must contain at least 3 characters")]
   public required string Name { get; set; }
-   [Required]
+  [Display(Name = "Office Email")]
+  [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+       ErrorMessage = "Invalid email format")]
+  [Required]
   public required string Email { get; set; }
   public string? PhotoPath { get; set; }
   public Dept? Department { get; set; }
