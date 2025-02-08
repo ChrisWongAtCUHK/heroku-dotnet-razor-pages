8bac7190a9e4f92a318623f61e1e107118f7a429
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-08 09:04:00 +0800

    Allow Chinese and Japanese charset

--- customers.sql
+++ customers.sql
@@ -8,6 +8,9 @@ DEFAULT CHARSET=latin1
 COLLATE=latin1_swedish_ci
 COMMENT='Customers';
 
+ALTER TABLE customers CHARACTER SET = utf8mb4 , COLLATE = utf8mb4_bin
+ALTER TABLE customers MODIFY COLUMN name varchar(100) CHARACTER SET utf8mb4
+
 INSERT INTO customers
 (id, name)
 VALUES(1, 'Customer 1');
