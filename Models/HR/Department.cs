48502e64c7837c37ae9dd457f5db2c0528bbefe2
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-09 19:18:36 +0800

    Add empty constructor

--- Models/HR/Department.cs
+++ Models/HR/Department.cs
@@ -5,14 +5,23 @@ using DotNetRazorPages.Entity;
 namespace DotNetRazorPages.Models.HR;
 
 [Table("departments")]
-public class Department(IRepository<Department> repository)
+public class Department
 {
   public int Id { get; set; }
   public required string Name { get; set; }
 
-  public required IRepository<Department>? _repository = repository;
+  public static IRepository<Department>? _repository;
 
-    public static async Task<Department?> ReadAsync(IRepository<Department> repository, int id)
+  public Department()
+  {
+  }
+
+  public Department(IRepository<Department> repository)
+  {
+    _repository = repository;
+  }
+
+  public static async Task<Department?> ReadAsync(IRepository<Department> repository, int id)
   {
     var department = await repository!.ReadAsync(id);
     return department;
