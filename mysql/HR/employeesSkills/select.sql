b85677400c9e489a4c66d0e797696396bfe6b9d3
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-10 11:57:46 +0800

    This is not space

--- mysql/HR/employeesSkills/select.sql
+++ mysql/HR/employeesSkills/select.sql
@@ -5,7 +5,7 @@ SELECT
   e.joinedDate,
   e.salary,
   d.name,
-  GROUP_CONCAT (s.title) AS `skills`
+  GROUP_CONCAT(s.title) AS `skills`
 FROM
   employees e
   INNER JOIN departments d ON d.id = e.departmentId
