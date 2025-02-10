0a0f73dbc5fd37eeb66728c26df9d90b547b8fd0
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-10 15:26:48 +0800

    More data in movies

--- mysql/movies/insert.sql
+++ mysql/movies/insert.sql
@@ -3,4 +3,16 @@ INSERT INTO
 VALUES
   (1, 'Movie 1', 'actorA, actorB'),
   (2, 'Movie 2', 'actorC, actorD'),
-  (3, 'Movie 3', 'actorE, actorF')
+  (3, 'Movie 3', 'actorE, actorF'),
+  (4, 'Movie 4', 'actorA, actor'),
+  (5, 'Movie 5', 'actorC, actorD'),
+  (6, 'Movie 6', 'actorE, actorG'),
+  (7, 'Movie 7', 'actorA, actorD'),
+  (8, 'Movie 8', 'actorC, actorB'),
+  (9, 'Movie 9', 'actorA, actorD'),
+  (10, 'Movie 10', 'actorA, actorE'),
+  (11, 'Movie 11', 'actorA, actorF'),
+  (12, 'Movie 12', 'actorB, actorE'),
+  (13, 'Movie 13', 'actorB, actorF'),
+  (14, 'Movie 14', 'actorC, actorF'),
+  (15, 'Movie 15', 'actorE, actorF')
