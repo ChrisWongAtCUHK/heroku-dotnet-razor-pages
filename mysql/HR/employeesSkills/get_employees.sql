327d7bf62a22bbb7226eddb01725d296434b38e7
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-10 12:03:06 +0800

    Create procedure get_employees

--- mysql/HR/employeesSkills/get_employees.sql
+++ mysql/HR/employeesSkills/get_employees.sql
@@ -1,8 +1,28 @@
--- DROP PROCEDURE IF EXISTS get_employee 
+DROP PROCEDURE IF EXISTS get_employees;
+
 DELIMITER $$
-CREATE PROCEDURE get_employee()  
+$$
+CREATE PROCEDURE get_employees()
 BEGIN
-	SELECT * FROM `employees` 
-END
-END $$
-DELIMITER ; -- this resets the semi-colon as a delimiter for further coding
+  SELECT
+    e.id,
+    e.firstName,
+    e.lastName,
+    e.joinedDate,
+    e.salary,
+    d.name,
+    GROUP_CONCAT(s.title) AS `skills`
+  FROM
+    employees e
+    INNER JOIN departments d ON d.id = e.departmentId
+    INNER JOIN employeesSkills es ON es.employeeId = e.id
+    INNER JOIN skills s ON s.id = es.skillId
+  GROUP BY
+    e.id
+  LIMIT
+    take
+  OFFSET
+    skip;
+END$$
+DELIMITER ;
+-- SHOW CREATE PROCEDURE get_employees
