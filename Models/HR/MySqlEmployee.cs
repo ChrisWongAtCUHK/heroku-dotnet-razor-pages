3308a9553be8d5392cc44489d03e363d2805531a
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-10 13:25:04 +0800

    Call get_employees

--- Models/HR/MySqlEmployee.cs
+++ Models/HR/MySqlEmployee.cs
@@ -1,8 +1,17 @@
 using System.ComponentModel.DataAnnotations;
 
 namespace DotNetRazorPages.Models.HR;
-public class MySqlEmployee : Employee
+public class MySqlEmployee
 {
+  public int Id { get; set; }
+  [Required]
+  public required string FirstName { get; set; }
+  [Required]
+  public required string LastName { get; set; }
+  [Required]
+  public required DateTime JoinedDate { get; set; }
+  [Required]
+  public required decimal Salary { get; set; }
   public required string Department { get; set; } 
-  public new required string Skills { get; set; }
+  public required string Skills { get; set; }
 }
