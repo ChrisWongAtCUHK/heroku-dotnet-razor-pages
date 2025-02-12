e8fcd0e5137376a38e483904cf8777a59be7b720
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-12 16:31:50 +0800

    Tidy up the code

--- Pages/Aspsnippets/Customers/Index.cshtml.cs
+++ Pages/Aspsnippets/Customers/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using DotNetRazorPages.Models.Aspsnippets;
@@ -6,16 +7,12 @@ using System.Data;
 
 namespace DotNetRazorPages.Pages.Aspsnippets;
 
-public class IndexModel : PageModel
+public class IndexModel(IWebHostEnvironment environment, IConfiguration configuration) : PageModel
 {
-    private IWebHostEnvironment Environment;
-    private IConfiguration Configuration;
-    public IndexModel(IWebHostEnvironment _environment, IConfiguration _configuration)
-    {
-        this.Environment = _environment;
-        this.Configuration = _configuration;
-    }
-    public List<Customer> Customer { get; set; }
+    private readonly IWebHostEnvironment _environment = environment;
+    private readonly IConfiguration _configuration = configuration;
+
+    public List<Customer> Customers { get; set; } = [];
 
     public void OnGet()
     {
@@ -23,11 +20,11 @@ public class IndexModel : PageModel
 
     public void OnPostSubmit(IFormFile postedFile)
     {
-        List<Customer> customers = new List<Customer>();
+        this.Customers = [];
         if (postedFile != null)
         {
             //Create a Folder.
-            string path = Path.Combine(this.Environment.WebRootPath, "Uploads");
+            string path = Path.Combine(_environment.WebRootPath, "Uploads");
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
@@ -37,31 +34,31 @@ public class IndexModel : PageModel
             string fileName = Path.GetFileName(postedFile.FileName);
             string filePath = Path.Combine(path, fileName);
             string extension = Path.GetExtension(postedFile.FileName);
-            using (FileStream stream = new FileStream(filePath, FileMode.Create))
+            using (FileStream stream = new (filePath, FileMode.Create))
             {
                 postedFile.CopyTo(stream);
             }
 
             using var doc = SpreadsheetDocument.Open(filePath, false);
             var workbookPart = doc.WorkbookPart;
-            var sheet = workbookPart.Workbook.Sheets.GetFirstChild<Sheet>();
-            var worksheet = ((WorksheetPart)workbookPart.GetPartById(sheet.Id)).Worksheet;
+            var sheet = workbookPart!.Workbook.Sheets!.GetFirstChild<Sheet>();
+            var worksheet = ((WorksheetPart)workbookPart.GetPartById(sheet!.Id!)).Worksheet;
             var worksheetName = sheet.Name;
             var sheetData = worksheet.GetFirstChild<SheetData>();
-            var sharedTable = workbookPart.SharedStringTablePart.SharedStringTable;
+            var sharedTable = workbookPart.SharedStringTablePart!.SharedStringTable;
             // 使用物件來承接資料
 
 
             // 跳過欄位名稱的 row1
-            foreach (var row in sheetData.ChildElements.Select(x => x as Row).Skip(1))
+            foreach (var row in sheetData!.ChildElements.Select(x => x as Row).Skip(1))
             {
                 var customer = new Customer();
 
-                for (var i = 0; i < row.ChildElements.Count; i++)
+                for (var i = 0; i < row!.ChildElements.Count; i++)
                 {
                     var cell = row.ChildElements[i] as Cell;
                     // 取得欄位資料
-                    var innerText = cell.InnerText;
+                    var innerText = cell!.InnerText;
                     // 只有字串的資料會在 SharedString Table
                     if (cell.DataType != null && cell.DataType == CellValues.SharedString)
                     {
@@ -83,12 +80,8 @@ public class IndexModel : PageModel
                     }
                 }
 
-
-                customers.Add(customer);
+                Customers.Add(customer);
             }
-
-
         }
-        this.Customer = customers;
     }
 }
