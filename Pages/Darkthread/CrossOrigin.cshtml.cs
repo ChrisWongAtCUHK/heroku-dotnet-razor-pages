ff6b43eb65f0edb2a960fe122763a8e5bc403c03
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-13 17:21:03 +0800

    Usr Razor Pages instead of html

---
+++ Pages/Darkthread/CrossOrigin.cshtml.cs
@@ -0,0 +1,18 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNetRazorPages.Pages.Darkthread;
+
+public class CrossOriginModel : PageModel
+{
+    public CrossOriginModel()
+    {
+        
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
---
+++ Pages/Darkthread/Parent.cshtml.cs
@@ -0,0 +1,18 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNetRazorPages.Pages.Darkthread;
+
+public class ParentModel : PageModel
+{
+    public ParentModel()
+    {
+        
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
