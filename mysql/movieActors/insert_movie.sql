7b8d752124e9fc45f57c174b9ad5e997c2e21594
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-24 17:36:30 +0800

    Insert movie by stored procedure

--- mysql/movieActors/insert_movie.sql
+++ mysql/movieActors/insert_movie.sql
@@ -2,10 +2,36 @@ DROP PROCEDURE IF EXISTS insert_movie;
 
 DELIMITER $$
 $$
-CREATE PROCEDURE insert_movie(movieName VARCHAR(100))
+CREATE PROCEDURE insert_movie(movieName VARCHAR(100), actors VARCHAR(65535))
 BEGIN
-  INSERT INTO movies (name) VALUES (movieName);
+	DECLARE remainder TEXT;
+	DECLARE delimiter CHAR(1);
+	DECLARE pos INT DEFAULT 1 ;
+	DECLARE str VARCHAR(1000);
+	DECLARE query varchar(65535);
+
+ 	-- INSERT INTO movies (name) VALUES (movieName);
+
+ 	SET delimiter = ',';
+	SET remainder = actors;
+	SET query = 'INSERT INTO actors (name) VALUES ';  
+	
+	WHILE CHAR_LENGTH(remainder) > 0 AND pos > 0 DO
+		SET pos = INSTR(remainder, `Delimiter`);
+		IF pos = 0 THEN
+			SET str = remainder;
+		ELSE
+			SET str = LEFT(remainder, pos - 1);
+		END IF;
+		IF TRIM(str) != '' THEN
+			SET query = CONCAT(query, '(', str, '), ');
+		END IF;
+		
+		SET remainder = SUBSTRING(remainder, pos + 1);
+	END WHILE;
+		
+	SELECT query;
 END$$
 DELIMITER ;
 -- SHOW CREATE PROCEDURE insert_movie
--- CALL insert_movie('Test')
+-- CALL insert_movie('Test movie', 'Actor 1, Actor 2, Actor 3')
