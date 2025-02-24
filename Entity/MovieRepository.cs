0da06cc55732d6015bf36511b3bfa6f643430c6e
Chris Wong <chris.wong.ksl@gmail.com>
2025-02-24 16:17:55 +0800

    Add single quote in sql

--- Entity/MovieRepository.cs
+++ Entity/MovieRepository.cs
@@ -15,7 +15,7 @@ public class MovieRepository<T>(MovieDbContext context) : Repository<T>(context)
         await Task.Run(() =>
         {
           // make sure insert_movie exists
-          set.FromSqlRaw<T>("CALL insert_movie({0})", movie!.Name);
+          set.FromSqlRaw<T>("CALL insert_movie('{0}')", movie!.Name);
         });
     }
 
